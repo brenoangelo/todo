d93426554255104c0b133dea120c81285f6bd9cb
Breno Angelo <brenoangeloal@gmail.com>
2022-08-23 22:23:26 -0300

    fix useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -2,9 +2,9 @@ export function useLocalStorage<T>(): [
   storage: T | null,
   setStorage: (key: string, value: T) => void,
 ] {
-  const itemStoraged: T | null = JSON.parse(
-    localStorage.getItem('tasks') ?? '',
-  );
+  const itemStoraged: T | null =
+    localStorage.getItem('tasks') &&
+    JSON.parse(localStorage.getItem('tasks') ?? '');
 
   function setStorage(key: string, value: T) {
     localStorage.setItem(key, JSON.stringify(value));
